b5c62aec7fad8e844a13c99cb2f84a853baf9c96
Seulgi Hwang <hsg151515@naver.com>
2024-06-18 22:31:07 +0900

    [Fix] 일정 확인 터치 이상한 부분 수정

--- js/home/swipe.js
+++ js/home/swipe.js
@@ -9,8 +9,8 @@ new Swiper('.swiper', {
 });
 
 new Swiper('.swiper2', {
-  // slidesPerView: 2, // 한번에 보여줄 슬라이드 개수
-  spaceBetween: -170, // 슬라이드 사이 여백
+  slidesPerView: 2, // 한번에 보여줄 슬라이드 개수
+  spaceBetween: 0, // 슬라이드 사이 여백
   centeredSlides: false, // 1번 슬라이드가 가운데 보이기
   freeMode: true,
 });
