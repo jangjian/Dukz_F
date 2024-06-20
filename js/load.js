0c3402b32fdbfe106460b612d07d82f60a43754f
sg-hwang-333 <hsg151515@naver.com>
2024-06-21 08:55:08 +0900

    [Fix] promise.all()로 로딩 구현

--- js/load.js
+++ js/load.js
@@ -1,7 +1,10 @@
 function showLoadingBar() {
-    document.getElementById('loadingBar').style.display = 'block';
-  }
-  
-  function hideLoadingBar() {
-    document.getElementById('loadingBar').style.display = 'none';
-  }
+  document.getElementById('loadingBar').style.display = 'block';
+}
+
+function hideLoadingBar() {
+  document.getElementById('loadingBar').style.display = 'none';
+}
+
+
+showLoadingBar();
