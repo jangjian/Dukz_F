9e9b8b233171ed018aceccabc0a10f1d95f9d081
Jian Jang <pureunnabi@gmail.com>
2024-06-20 23:41:03 +0900

    북마크 저장하기(완성X)

--- js/home/book_mark.js
+++ js/home/book_mark.js
@@ -7,4 +7,29 @@ function bookmarkChk(event, obj) {
         obj.src = "../../Image/icon/bookmark/un_fill.svg";
         // 카드뉴스를 북마크에서 해제
     }
+
+    const userid = localStorage.getItem('userid');
+    const cardNewsId = obj.getAttribute(''); 
+
+    console.log(userid, cardNewsId);
+
+    axios.post('http://54.180.238.52:3000/user/bookmarkCardNews', {
+        userid : userid,
+        cardNewsId: cardNewsId,
+    })
+    .then((response) => {
+        if (response.data.success) {
+            if (isBookmarked) {
+                console.log('북마크 해제됨');
+            } else {
+                element.classList.add('bookmarked');
+                console.log('북마크 추가됨');
+            }
+        } else {
+            console.error('북마크 업데이트에 실패했습니다.');
+        }
+    })
+    .catch((error) => {
+        console.error('북마크 업데이트 요청 중 오류 발생:', error);
+    });
 }
